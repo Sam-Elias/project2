f0e764c3f7a1376be687fb087ed673048288d838
Samir Elias <samirelias@gmail.com>
2018-05-27 02:01:11 -0700

    Add forEach loop + pseudocode

--- js/app.js
+++ js/app.js
@@ -3,15 +3,77 @@ const cards = Array.from(document.getElementsByClassName('card'));
 
 const openCards = Array.from(document.getElementsByClassName('open show'));
 
-const matchedCards = document.getElementsByClassName('match');
+const matchedCards = Array.from(document.getElementsByClassName('match'));
 
 
+//Loop through all cards and select the card clicked
+cards.forEach(function (el) {
+  el.addEventListener('click', clickFunction(el);
+}
+  /*
+  ---clickFunction---
+  //if
+  /*
+  *card is already open - display card is already open and return
+  */
+  //else if
+  /*
+  *card is already matched - display card is already matched and return
+  */
+  //else if
+  /*
+  *openCards.length is 0 - run _openCard function and return
+  */
+  //else if
+  /*
+  *openCards.length is 1 - run -openCard function, run _compareCard function and return
+  */
+  /*
+  --- _openCard Function---
+  *run _counter function
+  *add class 'open show' to element
+  ***** function(el) {
+  ***** el.classList.add('open', 'show');
+  */
+  /*
+  --- _compareCard Function---
+  *run _counter function
+  *get the two elements inside openCards array and compare (===)
+  *   if true run _matchClass function
+  *     check to see if matchedCards is === 16
+  *        if true game over + restart function
+  *   if false
+  *     run _removeClass function
+  */
+  /*
+  --- _removeClass function---
+  *for both elements of array openCards - remove class 'open show'
+  */
+  /*
+  --- _matchClass function---
+  *for both elements of array openCards - remove class 'open show' add class 'match'
+  */
+  /*
+  --- _gameOver function---
+  *run _modal function
+  *run _restart function
+  */
+  /*
+  --- _restart function---
+  *--set element with class number to 0
+  *--loop through array cards and get all elements with class 'open show'
+  *and all elements with class 'match' and then remove the class 'open show'
+  *and 'match' from them.
+  *--run suffle function on cards array
+  */
+  /*
+  --- _counter function---
+  *get element with class 'counter'
+  *add =+ 1 to the number being displayed
+  *update the element to show new number
 /*
  * Create a list that holds all of your cards
  */
-
-
-//const arrclose = Array.from(closeCard);
 
 
 /*
